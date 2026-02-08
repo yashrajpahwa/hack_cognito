b6548487e29f7ff6125f5cd74b49e6ca759bdc6b
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2026-02-08 07:53:09 +0530

    init

--- convert_to_js.py
+++
@@ -1,6 +0,0 @@
-﻿# Python script to create AgenticCity.jsx
-content = r'''CONTENT_PLACEHOLDER'''
-
-with open('AgenticCity.jsx', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('File created successfully')
